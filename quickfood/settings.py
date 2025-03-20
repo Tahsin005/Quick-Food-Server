40aeeb3ceee9f7b7c513378f9297d7d7aec1cf47
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-03-21 01:33:54 +0600

    api documentation using swagger

--- quickfood/settings.py
+++ quickfood/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     'corsheaders',
     'django_filters',
+    'drf_yasg',
 
     'users',
     'restaurants',
--- quickfood/urls.py
+++ quickfood/urls.py
@@ -17,9 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="QuickFood API",
+        default_version='v1',
+        description="QuickFood API documentation",
+    ),
+    public=True
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/', include('restaurants.urls')),
     path('api/', include('orders.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
